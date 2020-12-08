7dd0faf38cb2cd8edcba82704e240afdd8d7cc00
Jennifer Sanders <jennys1000@gmail.com>
2020-12-08 01:26:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-var kittens = [] //define your array here
-
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+function destructivelyAppendKitten(Zen) {
+ return kittens.push(Zen)
+}
 // Add your functions and code here
